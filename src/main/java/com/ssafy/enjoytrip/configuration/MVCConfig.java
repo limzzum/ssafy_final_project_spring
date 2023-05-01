fbdd6288eeb6dcadb0e6ae31a046b9ed000a6a80
tngks1995 <tngks1995@naver.com>
2023-05-01 16:46:27 +0900

    feat : MVCConfig.java

---
+++ src/main/java/com/ssafy/enjoytrip/configuration/MVCConfig.java
@@ -0,0 +1,18 @@
+package com.ssafy.enjoytrip.configuration;
+
+import com.ssafy.sakila.interceptor.SessionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+}
