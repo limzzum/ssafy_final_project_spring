064849ba4ae6a17a5d6f9848a84c525b00e69dc0
limzzum <chfhddl1234@naver.com>
2023-05-02 00:17:07 +0900

    feat : type, userNo 별 selectAll 메소드 추가

--- src/main/java/com/ssafy/enjoytrip/model/mapper/BoardMapper.java
+++ src/main/java/com/ssafy/enjoytrip/model/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package com.ssafy.enjoytrip.model.mapper;
 
 import com.ssafy.enjoytrip.model.dto.Board;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,6 +11,8 @@ public interface BoardMapper {
     int insert(Board board);
     Board select(int postId);
     List<Board> selectAll();
+    List<Board> selectAll(String type);
+    List<Board> selectAll(@Param("type") String type, @Param("userNo") int userNo);
     int delete(int postId);
     int update(Board board);
 
