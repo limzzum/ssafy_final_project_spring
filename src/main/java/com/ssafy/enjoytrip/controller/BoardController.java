aeb296340b3fd2c38c827b930d93b619842244c2
limzzum <chfhddl1234@naver.com>
2023-05-02 00:17:48 +0900

    feat : type, 사용자 별 boardList 요청 매핑 추가

--- src/main/java/com/ssafy/enjoytrip/controller/BoardController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.ssafy.enjoytrip.controller;
 import com.ssafy.enjoytrip.model.dto.Board;
 import com.ssafy.enjoytrip.model.dto.User;
 import com.ssafy.enjoytrip.model.service.BoardService;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,6 +21,26 @@ public class BoardController {
     private BoardService service;
     public BoardController(BoardService service){
         this.service = service;
+    }
+
+    @GetMapping("/list/{type}")
+    public ResponseEntity<Map<String, Object>> list(@PathVariable String type){
+
+        List<Board> list = service.getAll(type);
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", list);
+
+        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/list/{type}/my")
+    public ResponseEntity<Map<String, Object>> list(@PathVariable String type, HttpSession session){
+        User loginUser = (User) session.getAttribute("loginUser");
+        List<Board> list = service.getAll(type, loginUser.getUserNo());
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", list);
+
+        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/add")
