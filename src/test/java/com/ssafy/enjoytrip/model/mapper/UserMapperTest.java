3c9526998204301eee7006cda85c91922e18f638
tngks1995 <tngks1995@naver.com>
2023-05-01 17:28:57 +0900

    fix : gitignore

--- src/test/java/com/ssafy/enjoytrip/model/mapper/UserMapperTest.java
+++ src/test/java/com/ssafy/enjoytrip/model/mapper/UserMapperTest.java
@@ -5,6 +5,7 @@ import com.ssafy.enjoytrip.model.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -24,6 +25,14 @@ public class UserMapperTest {
     void selectTest(){
         User u = mapper.select("admin");
         assertEquals(u.getUserPwd(),"1234");
+    }
+
+    @Test
+    @Transactional
+    void insertTest(){
+        User u = User.builder().userId("testUser").userName("김테스트").userPwd("1234").email("test@ssafy.com").build();
+        mapper.insert(u);
+        assertEquals("1234",mapper.select("testUser").getUserPwd());
     }
 
 }
