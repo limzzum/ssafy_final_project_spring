3e2978e20c7b20490b734995469d9249ca5234f1
jeonghyun <chfhddl1234@naver.com>
2023-05-01 17:12:16 +0900

    test : update 테스트 추가

--- src/test/java/com/ssafy/enjoytrip/model/mapper/BoardMapperTest.java
+++ src/test/java/com/ssafy/enjoytrip/model/mapper/BoardMapperTest.java
@@ -56,5 +56,16 @@ class BoardMapperTest {
 
     @Test
     void update() {
+        Board board = Board.builder().title("title").content("content").userNo(1).boardType("review").hits(1).build();
+        mapper.insert(board);
+
+        board.setContent("change");
+        int result = mapper.update(board);
+        Assertions.assertEquals(result, 1);
+
+        Board selected = mapper.select(board.getPostId());
+
+        assertEquals(selected.getContent(), "change");
+
     }
 }
