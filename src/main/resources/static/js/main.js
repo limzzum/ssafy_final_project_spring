bf879d85f386dd2d6486e288f3aefce11b18473d
jeonghyun <chfhddl1234@naver.com>
2023-05-02 17:46:18 +0900

    feat : 관광지 검색 결과 리스트로 보여주기

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -28,24 +28,44 @@ function registUser(){
 //관광지 search
 
 async function searchPlace(){
-    let sido = document.querySelector("#search-area").value();
-    let type = document.querySelector("#search-content-id").value();
-    let keyword = document.querySelector("#search-keyword").value();
+    console.log("search")
+    let sido = document.getElementById("search-area").value
+    let type = document.getElementById("search-content-id").value
+    let keyword = document.getElementById("search-keyword").value
+    console.log(sido);
+    console.log(type);
+    console.log(keyword);
     if(!sido || !type || !keyword){
         alert("null값 안돼요");
         return;
     }
     let config = {
         method:"POST",
-        contentType:"application/json",
-        body:{
-            "sido":sido,
-            "type":type,
-            "keyword":keyword
-        }
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+            "sido":sido.toString(),
+            "type":type.toString(),
+            "keyword":keyword.toString()
+        })
         };
     let response = await fetch("/api/place/list", config);
     let json = await response.json();
-    console.log(json);
+    let result = json.result
+    console.log(result);
+    load([`themesearch`,`themetrip`]);
+    let tripList = document.querySelector("#trip-list").innerHTML
+    result.forEach(place =>{
+        let trip = `<tr>
+                    <td><img src="${place.image}" alt="" height="100px" onerror="this.src=/img/김민섭.PNG"/></td>
+                    <td>${place.title}</td>
+                    <td>${place.addr1}</td>
+                    <td>${place.lat}</td>
+                    <td>${place.lng}</td>
+                    </tr>`
+        tripList += trip;
+    })
+    document.querySelector("#trip-list").innerHTML = tripList;
 
 }
