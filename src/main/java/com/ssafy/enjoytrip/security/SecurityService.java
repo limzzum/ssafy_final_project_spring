ca89a1a8b8874bb3ba363407af548a02234f0754
limzzum <chfhddl1234@naver.com>
2023-05-15 16:21:10 +0900

    feat : jwt토큰을 생성 서비스 추가

---
+++ src/main/java/com/ssafy/enjoytrip/security/SecurityService.java
@@ -0,0 +1,41 @@
+package com.ssafy.enjoytrip.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class SecurityService {
+    private static final String SECRET_KEY = "wfaijelwfwwfajelwwlfjaleifajldcmsaqpoifejeliajwlfqpopoppoeimac";
+
+    public String createToken(String subject, long expTime){
+        if(expTime<=0){
+            throw new RuntimeException("만려시간은 0이상");
+        }
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(secretKeyBytes,signatureAlgorithm.getJcaName());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(signingKey, signatureAlgorithm)
+                .setExpiration(new Date(System.currentTimeMillis()+expTime))
+                .compact();
+    }
+
+    public String getSubject(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
