27c7b026bf6b8c72a2af535e81fe5ae2eb2af6d0
limzzum <chfhddl1234@naver.com>
2023-05-15 16:23:39 +0900

    feat : 로그인 인터셉터 추가

--- src/main/java/com/ssafy/enjoytrip/configuration/MVCConfig.java
+++ src/main/java/com/ssafy/enjoytrip/configuration/MVCConfig.java
@@ -1,11 +1,28 @@
 package com.ssafy.enjoytrip.configuration;
 
+import com.ssafy.enjoytrip.interceptor.LoginCheckInterCeptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterCeptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/api/user/regist", "/api/user/login", "/api/user/logout",
+                        "/css/**", "/*.ico", "/error/**");
+    }
+    @Bean
+    public LoginCheckInterCeptor loginCheckInterCeptor(){
+        return new LoginCheckInterCeptor();
+    }
+
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
