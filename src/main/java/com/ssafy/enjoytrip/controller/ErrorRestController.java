163b5b1f5f0928f4d3f3fc324b8b957239f5584d
jeonghyun <chfhddl1234@naver.com>
2023-05-16 17:42:53 +0900

    refactor : errorController ApiIgnore

--- src/main/java/com/ssafy/enjoytrip/controller/ErrorRestController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/ErrorRestController.java
@@ -5,12 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/error")
+@ApiIgnore
 public class ErrorRestController {
 
     @GetMapping("/login")
