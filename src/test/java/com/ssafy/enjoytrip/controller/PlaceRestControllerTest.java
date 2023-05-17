aae3f6c6b89749a2347419128f908e206988d559
jeonghyun <chfhddl1234@naver.com>
2023-05-17 11:54:14 +0900

    init

---
+++ src/test/java/com/ssafy/enjoytrip/controller/PlaceRestControllerTest.java
@@ -0,0 +1,18 @@
+package com.ssafy.enjoytrip.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletMapping;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceRestControllerTest {
+
+    @Test
+    void list() {
+
+    }
+
+    @Test
+    void detail() {
+    }
+}
