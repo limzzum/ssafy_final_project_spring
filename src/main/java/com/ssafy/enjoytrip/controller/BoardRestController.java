528c2e0e754a9c8c1ff7702c797313367f8da77c
jeonghyun <chfhddl1234@naver.com>
2023-05-19 12:32:50 +0900

    feat : boardController swagger 적용

--- src/main/java/com/ssafy/enjoytrip/controller/BoardRestController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/BoardRestController.java
@@ -5,6 +5,9 @@ import com.github.pagehelper.PageHelper;
 import com.ssafy.enjoytrip.model.dto.Board;
 import com.ssafy.enjoytrip.model.dto.User;
 import com.ssafy.enjoytrip.model.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/board")
 @Slf4j
+@Api(tags = "BOARD REST API")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BoardRestController {
     private final BoardService service;
@@ -27,8 +31,9 @@ public class BoardRestController {
         this.service = service;
     }
 
+    @ApiOperation(value = "board 상세 정보 요청", notes = "postId에 해당하는 board info 반환",response = ResponseEntity.class)
     @GetMapping("/{postId}")
-    public ResponseEntity<Map<String, Object>> list(@PathVariable int postId){
+    public ResponseEntity<Map<String, Object>> detail(@PathVariable @ApiParam(value = "board primarykey", required = true) int postId){
         Map<String,Object> map = new HashMap<>();
         Board select = service.select(postId);
         map.put("result", select);
@@ -36,8 +41,9 @@ public class BoardRestController {
         return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
     }
 
+    @ApiOperation(value = "board 수정 요청", notes = "title or content 수정 요청",response = ResponseEntity.class)
     @PutMapping
-    public ResponseEntity<Map<String, Object>> modify(@RequestBody Board board){
+    public ResponseEntity<Map<String, Object>> modify(@RequestBody @ApiParam(value = "수정할 board", required = true) Board board){
         log.info("modify");
         Map<String,Object> map = new HashMap<>();
         int update = service.update(board);
@@ -50,8 +56,10 @@ public class BoardRestController {
         map.put("msg",msg);
         return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
     }
+
+    @ApiOperation(value = "board 삭제 요청", notes = "postId에 해당하는 board 삭제",response = ResponseEntity.class)
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Map<String, Object>> delete(@PathVariable int postId){
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable @ApiParam(value = "삭제할 board id", required = true) int postId){
         log.info("delete");
         Map<String,Object> map = new HashMap<>();
         int delete = service.delete(postId);
@@ -64,20 +72,31 @@ public class BoardRestController {
         map.put("msg",msg);
         return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
     }
-    @GetMapping("/{type}/{page}")
-    public ResponseEntity<Map<String, Object>> list(@PathVariable String type,@PathVariable int page){
+
+    @ApiOperation(value = "board 리스트 요청", notes = "option값을 json으로 전달: title, content, boardType, user_no 전달 가능 ",response = ResponseEntity.class)
+    @PostMapping("/{page}")
+    public ResponseEntity<Map<String, Object>> list(@RequestBody @ApiParam(value = "boardList option값", required = true) Map<String,Object> body,
+                                                    @ApiParam(value = "page") @PathVariable int page){
         PageHelper.startPage(page,10);
-        Map<String,Object> keys = new HashMap<>(), map = new HashMap<>();
-        keys.put("boardType",type);
-        log.info("search : "+keys);
-        Page<Board> list = service.search(keys);
-        map.put("result", list);
-        map.put("pages",list.getPages());
-        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+        Map<String,Object> map = new HashMap<>();
+
+        try {
+            Page<Board> list = service.search(body);
+            map.put("result", list);
+            map.put("pages",list.getPages());
+            map.put("msg", "success");
+            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            e.printStackTrace();
+            map.put("msg", "fail");
+            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+        }
+
     }
 
+    @ApiOperation(value = "board 등록", notes = "등록할 board를 requestBody로 요청", response = ResponseEntity.class)
     @PostMapping
-    public ResponseEntity<Map<String, Object>> regist(@RequestBody Board board){
+    public ResponseEntity<Map<String, Object>> regist(@RequestBody @ApiParam(value = "등록할 board", required = true) Board board){
         log.info(String.valueOf(board.getUserNo()));
         log.info(board.getBoardType());
         int result = service.insert(board);
