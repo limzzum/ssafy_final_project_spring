c13640091893857d12c0482fb5f22fc98253907f
tngks1995 <tngks1995@naver.com>
2023-05-19 16:50:23 +0900

    INIT : legacy js files

---
+++ src/components/worldcup/WorldcupProgress.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "WorldcupProgress",
+  components: {},
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/worldcup/WorldcupResult.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "WorldcupResult",
+  components: {},
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
