e6ab6a5e3a2568b8345ff1a0247f0df3bf66b100
jeonghyun <chfhddl1234@naver.com>
2023-05-19 17:49:57 +0900

    feat :  테마여행 뷰에 searchbar 컴포넌트 추가

--- src/views/AppThemeTrip.vue
+++ src/views/AppThemeTrip.vue
@@ -1,11 +1,17 @@
 <template>
-  <b-container> Theme Trip </b-container>
+  <b-container> Theme Trip
+    <search-bar></search-bar>
+    
+    </b-container>
 </template>
 
 <script>
+import SearchBar from "@/components/themetrip/SearchBar.vue";
 export default {
   name: "AppThemeTrip",
-  components: {},
+  components: {
+    SearchBar
+  },
   data() {
     return {
       message: "",
