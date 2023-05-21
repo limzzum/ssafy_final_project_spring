7dc9a1236f5932136d766699d92405e649ad892b
limzzum <chfhddl1234@naver.com>
2023-05-21 17:10:21 +0900

    feat : themtripView 틀 작성

--- src/views/AppThemeTrip.vue
+++ src/views/AppThemeTrip.vue
@@ -2,15 +2,21 @@
   <b-container>
     Theme Trip
     <TheSearchbar />
+    <the-map />
+    <theme-list />
   </b-container>
 </template>
 
 <script>
 import TheSearchbar from "@/components/TheSearchbar.vue";
+import ThemeList from "@/components/themetrip/ThemeList.vue";
+import TheMap from "@/components/TheMap.vue";
 export default {
   name: "AppThemeTrip",
   components: {
     TheSearchbar,
+    ThemeList,
+    TheMap,
   },
   data() {
     return {
