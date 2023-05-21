0d44040637b67a7d25e18b7823a2fdb1cd79fdd8
limzzum <chfhddl1234@naver.com>
2023-05-22 00:32:42 +0900

    feat : pagenation vue 생성

---
+++ src/components/ThePagenation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="overflow-auto">
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
+
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      perPage: 10,
+      currentPage: 1,
+    };
+  },
+  computed: {
+    ...mapState("placeStore", ["places"]),
+    rows() {
+      return this.places.length;
+    },
+  },
+};
+</script>
