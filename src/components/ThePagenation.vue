2ca5d50dfac23436e7ecdcabe2a3b24566d54b5d
jeonghyun <chfhddl1234@naver.com>
2023-05-22 11:49:42 +0900

    feat : 페이지 변경에 따라 리스트 호출

--- src/components/ThePagenation.vue
+++ src/components/ThePagenation.vue
@@ -1,30 +1,51 @@
 <template>
   <div class="overflow-auto">
     <b-pagination
-      v-model="currentPage"
+      v-model="curPage"
       :total-rows="rows"
       :per-page="perPage"
       aria-controls="my-table"
     ></b-pagination>
 
-    <p class="mt-3">Current Page: {{ currentPage }}</p>
+    <p class="mt-3">Current Page: {{ curPage }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapMutations, mapActions } from "vuex";
 export default {
   data() {
     return {
       perPage: 10,
-      currentPage: 1,
+      curPage:1
     };
   },
   computed: {
-    ...mapState("placeStore", ["places"]),
+    ...mapState("placeStore", ["places", "totalPageNum"]),
+    
+
     rows() {
-      return this.places.length;
+      console.log("total "+this.totalPageNum)
+      return this.totalPageNum;
     },
   },
+  watch: {
+
+    curPage() {
+      console.log("change page")
+      this.setPage();
+      
+    }
+  },
+  methods: {
+    ...mapMutations("placeStore", ["SET_CURRENT_PAGE"]),
+    ...mapActions("placeStore", ["searchPlace"]),
+    setPage() {
+      console.log("change method")
+      this.SET_CURRENT_PAGE(this.curPage);
+      this.searchPlace();
+
+    }
+  }
 };
 </script>
