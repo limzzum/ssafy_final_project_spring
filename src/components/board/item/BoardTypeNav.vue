1b36503adfc5ec979e410e7055467cea0baeef7d
jeonghyun <chfhddl1234@naver.com>
2023-05-22 15:33:25 +0900

    feat : boad type nav 컴포넌트 추가

---
+++ src/components/board/item/BoardTypeNav.vue
@@ -0,0 +1,47 @@
+<template>
+    
+<b-button-group vertical
+style="border: 1px; width: 230px; height: 200px;">
+    <b-button @click="search('notice')" variant="outline-secondary">공지사항</b-button>
+<b-button @click="search('review')" variant="outline-secondary">여행후기</b-button>
+<b-button @click="search('free')" variant="outline-secondary">자유게시판</b-button>
+<b-button @click="search('team')" variant="outline-secondary">일행구하기</b-button>
+
+</b-button-group>
+  </template>
+  
+  <script>
+import { mapMutations, mapActions } from "vuex";
+const boardStore = "boardStore";
+  export default {
+    name: "BoardTypeNav",
+      data() {
+            return {
+                
+            };
+    },
+
+    methods: {
+    ...mapMutations(boardStore, ["SET_BOARD_TYPE"]),
+    ...mapActions(boardStore, [
+      "searchArticle",
+    ]),
+  
+    async search(type) {
+      console.log("search")
+          this.SET_BOARD_TYPE(type);
+      this.searchArticle();
+      // },
+      // TODO : search 후 /themetrip 이동 구현
+    },
+  },
+
+
+   
+  };
+  </script>
+  
+
+  
+  <style></style>
+  
