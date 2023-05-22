4f6a9c7c56967c83cf74dc3bf7e20f6a532c2d3d
jeonghyun <chfhddl1234@naver.com>
2023-05-22 14:25:37 +0900

    feat : 게시판 종류 선택 버튼그룹

--- src/views/AppBoard.vue
+++ src/views/AppBoard.vue
@@ -1,7 +1,18 @@
 <template>
-  <b-container> AppBoard 
-
-    <router-view/>
+  <b-container>  
+    <div style="display: flex;">
+      <b-button-group vertical 
+      style="border: 1px; width: 230px; height: 200px;">
+          <b-button variant="outline-secondary">공지사항</b-button>
+    <b-button variant="outline-secondary">여행후기</b-button>
+    <b-button variant="outline-secondary">자유게시판</b-button>
+    <b-button variant="outline-secondary">일행구하기</b-button>
+    
+  </b-button-group>
+      <router-view/>
+    </div>
+ 
+    
   </b-container>
 </template>
 
