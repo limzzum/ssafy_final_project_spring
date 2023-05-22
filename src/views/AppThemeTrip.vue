14fe965f586117f05ff72e86e8c86b4d8d626078
jeonghyun <chfhddl1234@naver.com>
2023-05-22 09:44:20 +0900

    refactor : searchbar ThemeList 컴포넌트에 포함시킴

--- src/views/AppThemeTrip.vue
+++ src/views/AppThemeTrip.vue
@@ -1,19 +1,17 @@
 <template>
   <b-container>
-    Theme Trip
-    <TheSearchbar />
+    <!-- <TheSearchbar /> -->
     <!-- <the-map /> -->
     <router-view />
   </b-container>
 </template>
 
 <script>
-import TheSearchbar from "@/components/TheSearchbar.vue";
+// import TheSearchbar from "@/components/TheSearchbar.vue";
 // import TheMap from "@/components/TheMap.vue";
 export default {
   name: "AppThemeTrip",
   components: {
-    TheSearchbar,
     // TheMap,
   },
   data() {
