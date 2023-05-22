deb6dd0d155c417fee09c4db8dab8c368387f588
jeonghyun <chfhddl1234@naver.com>
2023-05-22 13:22:53 +0900

    fix : api 요청 경로 수정

--- src/api/board.js
+++ src/api/board.js
@@ -2,26 +2,26 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
-function listArticle(page, success, fail) {
-  api.get(`/api/board/review/${page}`).then(success).catch(fail);
+function listArticle(page,config, success, fail) {
+  api.post(`/board/search/${page}`, JSON.stringify(config)).then(success).catch(fail);
 }
 
 function writeArticle(article, success, fail) {
   console.log(article);
-  api.post(`/api/board`, JSON.stringify(article)).then(success).catch(fail);
+  api.post(`/board`, JSON.stringify(article)).then(success).catch(fail);
 }
 
 function getArticle(postId, success, fail) {
   console.log(postId)
-  api.get(`/api/board/${postId}`).then(success).catch(fail);
+  api.get(`/board/${postId}`).then(success).catch(fail);
 }
 
 function modifyArticle(article, success, fail) {
-  api.put(`/api/board`, JSON.stringify(article)).then(success).catch(fail);
+  api.put(`/board`, JSON.stringify(article)).then(success).catch(fail);
 }
 
 function deleteArticle(postId, success, fail) {
-  api.delete(`/api/board/${postId}`).then(success).catch(fail);
+  api.delete(`/board/${postId}`).then(success).catch(fail);
 }
 
 export { listArticle, writeArticle, getArticle, modifyArticle, deleteArticle };
