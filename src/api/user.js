d09b8a4c8042e9d2a81bc0f5f30a166af71c788e
jeonghyun <chfhddl1234@naver.com>
2023-05-23 13:29:00 +0900

    refactor : 리프레시 요청 메서드 수정

--- src/api/user.js
+++ src/api/user.js
@@ -12,10 +12,9 @@ async function findByUserNo(userNo, success, fail) {
   await api.get(`/user/select/${userNo}`).then(success).catch(fail);
 }
 
-async function tokenRegeneration(user, success, fail) {
-  api.defaults.headers["refresh-token"] =
-    sessionStorage.getItem("refresh-token"); //axios header에 refresh-token 셋팅
-  await api.post(`/user/refresh`, user).then(success).catch(fail);
+async function tokenRegeneration( success, fail) {
+  api.defaults.headers["refresh-token"] = sessionStorage.getItem("refresh-token"); //axios header에 refresh-token 셋팅
+  await api.get(`/user/refresh`).then(success).catch(fail);
 }
 
 async function regist(user, success, fail) {
