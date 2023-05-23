96255548c562a61681510c2382b950e5daa31902
jeonghyun <chfhddl1234@naver.com>
2023-05-23 13:52:21 +0900

    feat : boardType 선택에 따라 title값 넘겨주기

--- src/components/board/item/BoardTypeNav.vue
+++ src/components/board/item/BoardTypeNav.vue
@@ -2,10 +2,11 @@
     
 <b-button-group vertical
 style="border: 1px; width: 230px; height: 200px;">
-    <b-button @click="search('notice')" variant="outline-secondary">공지사항</b-button>
-<b-button @click="search('review')" variant="outline-secondary">여행후기</b-button>
-<b-button @click="search('free')" variant="outline-secondary">자유게시판</b-button>
-<b-button @click="search('team')" variant="outline-secondary">일행구하기</b-button>
+    <b-button @click="search('notice','공지사항')" variant="outline-secondary">공지사항</b-button>
+<b-button @click="search('review','여행후기')" variant="outline-secondary">여행후기</b-button>
+<b-button @click="search('free','자유게시판')" variant="outline-secondary">자유게시판</b-button>
+<b-button @click="search('team','일행구하기')" variant="outline-secondary">일행구하기</b-button>
+<b-button @click="search('plan','여행코스')" variant="outline-secondary">여행 코스</b-button>
 
 </b-button-group>
   </template>
@@ -22,15 +23,16 @@ const boardStore = "boardStore";
     },
 
     methods: {
-    ...mapMutations(boardStore, ["SET_BOARD_TYPE"]),
+    ...mapMutations(boardStore, ["SET_BOARD_TYPE","SET_BOARD_TITLE"]),
     ...mapActions(boardStore, [
       "searchArticle",
     ]),
   
-    async search(type) {
+    async search(type,title) {
       console.log("search")
       
       this.SET_BOARD_TYPE(type);
+      this.SET_BOARD_TITLE(title);
       this.searchArticle();
       
       this.$router.push({ name: "boardlist" }).catch(() => { });
