995364d5c8860ebd2e4194251423d8683b175c81
jeonghyun <chfhddl1234@naver.com>
2023-05-23 10:26:05 +0900

    feat : 로그인 체크 인터셉터 다시 추가

--- src/main/java/com/ssafy/enjoytrip/configuration/MVCConfig.java
+++ src/main/java/com/ssafy/enjoytrip/configuration/MVCConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class MVCConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(loginCheckInterCeptor())
-//                .order(1)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/", "/api/user/regist", "/api/user/login", "/api/user/logout/**","/api/place/**",
-//                        "/**/*.css","/**/*.jpg","/**/*.jpeg","/**/*.png","/**/*.peg", "/**/*.js", "/error/**",
-//                        "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**");
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterCeptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/api/user/regist", "/api/user/login", "/api/user/logout/**","/api/place/**",
+                        "/**/*.css","/**/*.jpg","/**/*.jpeg","/**/*.png","/**/*.peg", "/**/*.js", "/error/**",
+                        "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**");
+    }
 
     @Bean
     public LoginCheckInterCeptor loginCheckInterCeptor(){
