cb2a125c50df6f62d2780268c94b9adfac2884d6
jeonghyun <chfhddl1234@naver.com>
2023-05-24 09:24:32 +0900

    refactor : AllArgsConstuctor 어노테이션 추가

--- src/main/java/com/ssafy/enjoytrip/model/dto/form/LoginForm.java
+++ src/main/java/com/ssafy/enjoytrip/model/dto/form/LoginForm.java
@@ -1,10 +1,12 @@
-package com.ssafy.enjoytrip.model.dto.valid;
+package com.ssafy.enjoytrip.model.dto.form;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@AllArgsConstructor
 public class LoginForm {
     @NotEmpty
     private String userId;
