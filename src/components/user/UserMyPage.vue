35445f0650b51b6133387258cfea69b83f85c1b3
jeonghyun <chfhddl1234@naver.com>
2023-05-24 11:45:11 +0900

    feat : mypage 화면 추가

--- src/components/user/UserMyPage.vue
+++ src/components/user/UserMyPage.vue
@@ -1,68 +1,151 @@
 <template>
   <b-container class="mt-4" v-if="userInfo">
     <b-row>
-      <b-col>
-        <b-alert variant="secondary" show><h3>내정보</h3></b-alert>
-      </b-col>
+      <div>
+  <b-tabs content-class="mt-3">
+    <b-tab title="내 프로필" active>
+      <table style="width: 300%; text-align: start; ">
+        <thead>
+          <th>아이디</th>
+        <th>이름</th>
+        <th>이메일</th>
+        </thead>
+        <tbody >
+          <tr style="border-bottom: 1px solid rgb(228, 223, 223); border-top: 1px solid rgb(228, 223, 223);">
+          <td >{{ userInfo.userId }}</td>
+          <td>{{ userInfo.userName }}</td>
+          <td>{{ userInfo.email }}</td>
+          </tr>
+          
+        </tbody>
+       
+      </table>
+
+    </b-tab>
+    <b-tab @click="routeMyBoard()" title="작성한 글 보기">
+  
+    <div style="display: flex;width: 170%;">
+        <BoardTypeNav :isMy="true"/>
+      <router-view/>
+    </div>
+    </b-tab>
+ 
+    <b-tab title="프로필 정보 수정" >
+   
+
+
+
+                <div class="col-12">
+                 
+                        <table class="table table-borderless display-flex p-3"
+                               style="text-align: center; vertical-align: middle; width:170%">
+                            <thead>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td colspan="2">
+                                    <label for="originalpw">기존 비밀번호</label>
+                                    <input v-model="user.curPwd" type="password" class="form-control" id="originalpw" name="userPwd"
+                                           placeholder="" required/>
+                                </td>
+                                <td rowspan="2">
+                                    <button type="button" class="btn btn-primary btn-lg btn-block m-1"
+                                            id="changePW" v-on:click="updateUser()">비밀번호 변경
+                                    </button>
+                                </td>
+                                
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="newPw">새 비밀번호</label>
+                                    <input v-model="user.newPwd" type="password" class="form-control" id="newPw" name="newPwd" placeholder=""
+                                           required/>
+                                </td>
+                                <td>
+                                    <label for="newPwCheck">새 비밀번호 확인</label>
+                                    <input v-model="user.checkPwd" type="password" class="form-control" id="newPwCheck" placeholder=""
+                                           required/>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+             <button v-on:click="updateUser()"></button>
+
+                    <div class="col-md">
+                       
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input"
+                                       id="deleteConfirm" required/>
+                                <label class="custom-control-label" for="deleteConfirm">회원 탈퇴시 저장된 회원 정보가 사라지는 것을
+                                    이해했으며, 회원 탈퇴에 동의합니다.</label>
+                            </div>
+                            <button class="btn btn-primary btn-lg btn-danger btn-block m-1"
+                                    id="deleteUser">회원 탈퇴
+                            </button>
+                    
+                    </div>
+                </div>
+
+    </b-tab>
+  </b-tabs>
+</div>
     </b-row>
     <b-row>
       <b-col></b-col>
-      <b-col cols="8">
-        <b-jumbotron>
-          <template #header>My Page</template>
-
-          <template #lead> 내 정보 확인페이지입니다. </template>
-
-          <hr class="my-4" />
-
-          <b-container class="mt-4">
-            <b-row>
-              <b-col cols="2"></b-col>
-              <b-col cols="2" align-self="end">아이디</b-col
-              ><b-col cols="4" align-self="start">{{ userInfo.userid }}</b-col>
-              <b-col cols="2"></b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="2"></b-col>
-              <b-col cols="2" align-self="end">이름</b-col
-              ><b-col cols="4" align-self="start">{{ userInfo.username }}</b-col>
-              <b-col cols="2"></b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="2"></b-col>
-              <b-col cols="2" align-self="end">이메일</b-col
-              ><b-col cols="4" align-self="start">{{ userInfo.email }}</b-col>
-              <b-col cols="2"></b-col>
-            </b-row>
-            <b-row>
-              <b-col cols="2"></b-col>
-              <b-col cols="2" align-self="end">가입일</b-col
-              ><b-col cols="4" align-self="start">{{ userInfo.joindate }}</b-col>
-              <b-col cols="2"></b-col>
-            </b-row>
-          </b-container>
-          <hr class="my-4" />
-
-          <b-button variant="primary" href="#" class="mr-1">정보수정</b-button>
-          <b-button variant="danger" href="#">회원탈퇴</b-button>
-        </b-jumbotron>
-      </b-col>
+      
       <b-col></b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import { mapState } from "vuex";
-
+import { mapState, mapActions } from "vuex";
+// import BoardList from "@/components/board/BoardList.vue";
+// import UserBoardList from "@/components/user/UserBoardList.vue";
+import BoardTypeNav from "../board/item/BoardTypeNav.vue";
 const userStore = "userStore";
 
 export default {
   name: "UserMyPage",
-  components: {},
+  components: { BoardTypeNav},
+  data() {
+    return {
+      user: {
+        curPwd: null,
+        newPwd: null,
+        checkPwd: null,
+      }
+      
+    }
+  },
   computed: {
     ...mapState(userStore, ["userInfo"]),
+    
+ 
   },
+  methods:{
+    ...mapActions(userStore, ["userUpdate"]),
+    updateUser() {
+      console.log(this.user.curPwd , this.user.newPwd , this.user.checkPwd)
+      if (this.user.curPwd & this.user.newPwd & this.user.checkPwd) {
+        if (this.user.newPwd === this.user.checkPwd) {
+          let newUser = {
+            "userNo": sessionStorage.getItem("userNo"),
+            "curPwd": this.user.curPwd,
+            "newPwd": this.user.newPwd
+          };
+          this.userUpdate(newUser);
+          this.user.curPwd = null;
+          this.user.newPwd = null;
+          this.user.checkPwd = null;
+        }
+     
+      }
+    },
+    routeMyBoard() {
+      this.$router.push({ name: "myboardlist" ,props:{isMy:true}});
+    }
+  }
 };
 </script>
 
