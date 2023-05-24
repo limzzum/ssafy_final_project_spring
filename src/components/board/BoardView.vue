4b2b60e30c3ebfa9262caee0a63bb51a90eced44
tngks1995 <tngks1995@naver.com>
2023-05-24 14:04:27 +0900

    fix : 게시판

---
+++ src/components/board/BoardView.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-container>
+    <b-card border-variant="black" class="mb-2" no-body>
+      <b-card-header>
+        <b-row class="text-center">
+          <b-col cols="3">{{ article.boardTitle }}</b-col>
+          <b-col> | </b-col>
+          <b-col cols="8">{{ article.title }}</b-col>
+        </b-row>
+        <b-row class="text-center">
+          <b-col cols="4">
+            {{ article.userName }}
+            | {{ regTime }}
+          </b-col>
+          <b-col cols="8">
+            <BIconEye variant="secondary"></BIconEye> {{ article.hits }}
+            <BIconChatDots variant="secondary"></BIconChatDots> 0
+          </b-col>
+        </b-row>
+      </b-card-header>
+      <b-card-body class="text-left">
+        <div v-html="message"></div>
+      </b-card-body>
+    </b-card>
+  </b-container>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import moment from "moment";
+const boardStore = "boardStore";
+export default {
+  name: "BoardView",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(boardStore, ["article"]),
+    regTime() {
+      return moment(this.article.regTime).format("YY.MM.DD hh:mm");
+    },
+  },
+  methods: {
+    ...mapActions(boardStore, []),
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/board/BoardWrite.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-container>
+    <h1>aa</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "BoardWrite",
+  components: {},
+};
+</script>
+
+<style></style>
