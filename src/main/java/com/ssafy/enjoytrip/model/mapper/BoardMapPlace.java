b44cb1c83fe9ff082a101cb6e9031d16585d71d8
jeonghyun <chfhddl1234@naver.com>
2023-05-24 17:50:30 +0900

    feat : BoardMapPlace mapper 생성

---
+++ src/main/java/com/ssafy/enjoytrip/model/mapper/BoardMapPlace.java
@@ -0,0 +1,14 @@
+package com.ssafy.enjoytrip.model.mapper;
+
+import com.ssafy.enjoytrip.model.dto.Place;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface BoardMapPlace {
+    int insert(Map<String, Object> map);
+    List<Place> search(int postId);
+    int delete(int postId);
+}
