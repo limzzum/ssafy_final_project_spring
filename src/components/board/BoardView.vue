1108744686b013060251a095d4804390f3c02f5f
tngks1995 <tngks1995@naver.com>
2023-05-24 14:12:49 +0900

    fix : 게시판

--- src/components/board/BoardView.vue
+++ src/components/board/BoardView.vue
@@ -1,27 +1,49 @@
 <template>
-  <b-container>
-    <b-card border-variant="black" class="mb-2" no-body>
-      <b-card-header>
-        <b-row class="text-center">
-          <b-col cols="3">{{ article.boardTitle }}</b-col>
-          <b-col> | </b-col>
-          <b-col cols="8">{{ article.title }}</b-col>
-        </b-row>
-        <b-row class="text-center">
-          <b-col cols="4">
-            {{ article.userName }}
-            | {{ regTime }}
-          </b-col>
-          <b-col cols="8">
-            <BIconEye variant="secondary"></BIconEye> {{ article.hits }}
-            <BIconChatDots variant="secondary"></BIconChatDots> 0
-          </b-col>
-        </b-row>
-      </b-card-header>
-      <b-card-body class="text-left">
-        <div v-html="message"></div>
-      </b-card-body>
-    </b-card>
+  <b-container class="m-3 p-2">
+    <b-row>
+      <b-col>
+        <b-card border-variant="black" class="mb-2" no-body>
+          <b-card-header>
+            <b-row>
+              <b-col class="board-title" cols="2">{{
+                article.boardTitle
+              }}</b-col>
+              <b-col class="board-title"> | </b-col>
+              <b-col class="article-title" cols="8">{{ article.title }}</b-col>
+            </b-row>
+            <b-row>
+              <b-col class="article-info" cols="3">
+                {{ article.userName }}
+                | {{ regTime }}
+              </b-col>
+              <b-col class="article-icon" cols="8">
+                <BIconEye variant="secondary"></BIconEye> {{ article.hits }}
+                <BIconChatDots variant="secondary"></BIconChatDots> 0
+              </b-col>
+            </b-row>
+          </b-card-header>
+          <b-card-body class="article-body">
+            <b-row>
+              <b-col>
+                {{ article.content }}
+              </b-col>
+            </b-row>
+          </b-card-body>
+          <b-card-footer>
+            <b-row>
+              <b-col> 등록된 댓글이 없습니다. </b-col>
+            </b-row>
+          </b-card-footer>
+        </b-card>
+      </b-col>
+    </b-row>
+    <b-row class="text-center">
+      <b-col>
+        <router-link to="/board/list">
+          <b-button variant="primary"> 목록 </b-button>
+        </router-link>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -47,4 +69,25 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.board-title {
+  font-size: x-large;
+  text-align: center;
+  color: cornflowerblue;
+}
+.article-title {
+  font-size: x-large;
+  text-align: left;
+}
+.article-info {
+  text-align: center;
+  color: grey;
+}
+.article-icon {
+  text-align: right;
+  color: grey;
+}
+.article-body {
+  min-height: 300px;
+}
+</style>
