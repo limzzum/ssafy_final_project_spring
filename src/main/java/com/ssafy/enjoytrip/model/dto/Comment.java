d44baf43f4c50a91b624d77c4454ace155042b6b
limzzum <chfhddl1234@naver.com>
2023-05-24 21:40:38 +0900

    refactor : comment userNo 컬럼 추가

--- src/main/java/com/ssafy/enjoytrip/model/dto/Comment.java
+++ src/main/java/com/ssafy/enjoytrip/model/dto/Comment.java
@@ -5,7 +5,8 @@ import java.sql.Timestamp;
 public class Comment {
     int commentId; // AI, PK
     int postId;
-    int parentId;
+    int parentId; //0이 아니면 대댓글
+    int userNo;
     String content;
     Timestamp createTime;
 
