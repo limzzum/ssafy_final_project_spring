a0a6b7158c6795345b5e1a09359ea40cf7f2a2df
tngks1995 <tngks1995@naver.com>
2023-05-25 13:45:21 +0900

    feat : draggable

--- src/views/AppMyPlan.vue
+++ src/views/AppMyPlan.vue
@@ -2,10 +2,13 @@
   <b-container
     ><PlaceSearchbar class="m-2" />
     <b-row>
-      <b-col cols="7" class="text-center"
-        ><PlaceList :viewDetail="false"
-      /></b-col>
-      <b-col cols="5" class="p-3"><TheMap :markSelected="true" /></b-col>
+      <b-col cols="8" class="text-center">
+        <TheMap />
+        <PlaceList :viewDetail="false" />
+      </b-col>
+      <b-col cols="4" class="p-3">
+        <PlaceSelect />
+      </b-col>
     </b-row>
   </b-container>
 </template>
@@ -14,10 +17,11 @@
 import PlaceSearchbar from "@/components/place/PlaceSearchbar.vue";
 import PlaceList from "@/components/place/PlaceList.vue";
 import TheMap from "@/components/TheMap.vue";
+import PlaceSelect from "@/components/place/PlaceSelect.vue";
 
 export default {
   name: "AppMyPlan",
-  components: { TheMap, PlaceList, PlaceSearchbar },
+  components: { TheMap, PlaceList, PlaceSearchbar, PlaceSelect },
   data() {
     return {
       message: "",
