36e4ee0c516ef04e6e857458fe9cbd5974f468ae
jeonghyun <chfhddl1234@naver.com>
2023-05-25 13:29:47 +0900

    feat : comment controller 추가

--- src/main/java/com/ssafy/enjoytrip/controller/CommentRestController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/CommentRestController.java
@@ -3,9 +3,11 @@ package com.ssafy.enjoytrip.controller;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.ssafy.enjoytrip.model.dto.Board;
+import com.ssafy.enjoytrip.model.dto.Comment;
 import com.ssafy.enjoytrip.model.dto.form.PostBoard;
 import com.ssafy.enjoytrip.model.service.BoardMapPlaceService;
 import com.ssafy.enjoytrip.model.service.BoardService;
+import com.ssafy.enjoytrip.model.service.CommentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -15,9 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/comment")
@@ -25,105 +25,113 @@ import java.util.Map;
 @Api(tags = "BOARD REST API")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CommentRestController {
-    private final BoardService service;
-    private final BoardMapPlaceService boardMapPlaceService;
+    private final CommentService service;
 
     @Autowired
-    public CommentRestController(BoardService service, BoardMapPlaceService boardMapPlaceService){
+    public CommentRestController(CommentService service){
         this.service = service;
-        this.boardMapPlaceService = boardMapPlaceService;
     }
 
-    @PostMapping("/test")
-    public ResponseEntity<Map<String, Object>> test(@RequestBody Map<String,Object> body){
-        List list= (List) body.get("list");
-
-        log.info(list.toString());
-        return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
-    }
-    @ApiOperation(value = "board 상세 정보 요청", notes = "postId에 해당하는 board info 반환",response = ResponseEntity.class)
-    @GetMapping("/{postId}")
-    public ResponseEntity<Map<String, Object>> detail(@PathVariable @ApiParam(value = "board primarykey", required = true) int postId){
-        Map<String,Object> map = new HashMap<>();
-        Board select = service.select(postId);
-        map.put("result", select);
-
-        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
-    }
-
-    @ApiOperation(value = "board 수정 요청", notes = "title or content 수정 요청",response = ResponseEntity.class)
-    @PutMapping
-    public ResponseEntity<Map<String, Object>> modify(@RequestBody @ApiParam(value = "수정할 board", required = true) Board board){
-        Map<String,Object> map = new HashMap<>();
-        int update = service.update(board);
-        String msg = "success";
-        if(update==1){
-            map.put("msg",msg);
-            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
-        }
-        msg = "fail";
-        map.put("msg",msg);
-        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
-    }
-
-    @ApiOperation(value = "board 삭제 요청", notes = "postId에 해당하는 board 삭제",response = ResponseEntity.class)
-    @DeleteMapping("/{postId}")
-    public ResponseEntity<Map<String, Object>> delete(@PathVariable @ApiParam(value = "삭제할 board id", required = true) int postId){
-        Map<String,Object> map = new HashMap<>();
-        int delete = service.delete(postId);
-        String msg = "success";
-        if(delete==1){
-            map.put("msg",msg);
-            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
-        }
-        msg = "fail";
-        map.put("msg",msg);
-        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
-    }
-
-    @ApiOperation(value = "board 리스트 요청", notes = "option값을 json으로 전달: title, content, boardType, userNo, userName 전달 가능 ",response = ResponseEntity.class)
-    @PostMapping("/search/{page}")
-    public ResponseEntity<Map<String, Object>> list(@RequestBody @ApiParam(value = "boardList option값", required = true) Map<String,Object> body,
-                                                    @ApiParam(value = "page") @PathVariable int page){
-        PageHelper.startPage(page,10);
-        Map<String,Object> map = new HashMap<>();
-
-        try {
-            Page<Board> list = service.search(body);
-            map.put("result", list);
-            map.put("pages",list.getPages());
-            map.put("totalNum",list.getTotal());
-            map.put("msg", "success");
-            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
-        }catch (Exception e){
-            e.printStackTrace();
-            map.put("msg", "fail");
-            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
-        }
-
-    }
-
-    @ApiOperation(value = "board 등록", notes = "등록할 board를 requestBody로 요청", response = ResponseEntity.class)
+    @ApiOperation(value = "댓글 등록", notes = "", response = ResponseEntity.class)
     @PostMapping
-    public ResponseEntity<Map<String, Object>> regist(@RequestBody @ApiParam(value = "등록할 board", required = true) PostBoard board){
+    public ResponseEntity<Map<String, Object>> regist(@RequestBody @ApiParam(value = "등록할 comment", required = true) Comment comment){
         String  msg = "success";
         try{
-            service.insert(board);
-            Map<String , Object> map = new HashMap<>();
-            if(board.getPlaces()!=null && board.getPlaces().size()!=0){
-                map.put("postId", board.getPostId());
-                map.put("places", board.getPlaces());
-                boardMapPlaceService.insert(map);
-            }
+            service.insert(comment);
         }catch (Exception e){
             e.printStackTrace();
             msg = "fail";
         }
-
         Map<String, Object> map = new HashMap<>();
         map.put("msg", msg);
         return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
     }
+
+    @ApiOperation(value = "댓글 수정 요청", notes = "",response = ResponseEntity.class)
+    @PutMapping
+    public ResponseEntity<Map<String, Object>> modify(@RequestBody @ApiParam(value = "수정할 댓글", required = true) Comment comment){
+        String  msg = "success";
+        try{
+            service.update(comment);
+        }catch (Exception e){
+            e.printStackTrace();
+            msg = "fail";
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", msg);
+        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+    }
+    @ApiOperation(value = "댓글 삭제 요청", notes = "",response = ResponseEntity.class)
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable @ApiParam(value = "삭제할 board id", required = true) int commentId){
+        Map<String,Object> map = new HashMap<>();
+        Comment select = service.select(commentId);
+        String msg = "fail";
+        if(select.getSubs().size() != 0){
+            map.put("result","대댓글이 있어 삭제할 수 없습니다.");
+        }else {
+        try {
+            service.delete(commentId);
+            msg = "success";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        }
+        map.put("msg",msg);
+        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+
+    }
+
+//    @ApiOperation(value = "board 에 달린 댓글 요청", notes = "postId에 해당하는 댓글 반환",response = ResponseEntity.class)
+//    @PostMapping
+//    public ResponseEntity<Map<String, Object>> lista(@RequestBody @ApiParam(value = "comment option(postId, userNo, content 입력 가능)", required = true) Map<String, Object> body
+//                                                    ){
+//        Map<String,Object> map = new HashMap<>();
+//        body.put("parentId",0);
+//        List<Comment> search = service.search(body);
+//
+//        Comment[][] list = new Comment[search.size()][];
+//        System.out.println(list);
+//        List<Comment> comments = search;
+//        System.out.println(comments);
+//
+//        for (int i = 0; i < search.size(); i++) {
+//            body.put("parentId", search.get(i).getCommentId());
+//            List<Comment> sub = service.search(body);
+//            comments.get(i).setSubs(sub);
+//        }
+//        map.put("result", comments);
+//
+//        return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+//    }
+//
+
+//
+//
+//
+//    @ApiOperation(value = "board 리스트 요청", notes = "option값을 json으로 전달: title, content, boardType, userNo, userName 전달 가능 ",response = ResponseEntity.class)
+//    @PostMapping("/search/{page}")
+//    public ResponseEntity<Map<String, Object>> list(@RequestBody @ApiParam(value = "boardList option값", required = true) Map<String,Object> body,
+//                                                    @ApiParam(value = "page") @PathVariable int page){
+//        PageHelper.startPage(page,10);
+//        Map<String,Object> map = new HashMap<>();
+//
+//        try {
+//            Page<Board> list = service.search(body);
+//            map.put("result", list);
+//            map.put("pages",list.getPages());
+//            map.put("totalNum",list.getTotal());
+//            map.put("msg", "success");
+//            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            map.put("msg", "fail");
+//            return new ResponseEntity<>(map, HttpStatus.ACCEPTED);
+//        }
+//
+//    }
+//
+//
 
 
 
